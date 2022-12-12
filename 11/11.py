589bd7d78ed6ca0aa3ad7bb9c08047479459d2b2
Chase Clark <chase_clark07@hotmail.com>
2022-12-11 21:48:47 -0500

    day 11 start

--- 11/11.py
+++ 11/11.py
@@ -1,2 +1,14 @@
 with open("./11/input2.txt") as f:
     lines = f.read().strip().split('\n')
+
+class monkey:
+    def __init__(self,id: int, items: list, operation: function, test) -> None:
+        self.id = id
+        self.items = items
+        self.operation = operation # maybe make this its own class?
+        self.test = test # maybe make this its own class?
+
+    def __str__(self) -> str:
+        pass
+
+# parse text file into n monkey objects
